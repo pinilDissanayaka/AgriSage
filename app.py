b2ecdbc450bd82d74459d4989b4544da9059ad8f
= <=>
2024-03-01 22:35:14 +0530

    log in eka hari

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, url_for, redirect, request, render_template
+from flask import Flask, url_for, redirect, request, render_template, session
+from utils.user import User
 
 app=Flask(__name__)
+user=User(app=app)
+app.secret_key='123456'
+
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
+def login(errorMassage=" "):
+    if request.method=='POST':
+        uName=request.form['username']
+        password=request.form['password']
+        
+        errorMassage,loggedUser = user.logInUser(emailAddress=uName, password=password)
+        
+        if loggedUser is None:
+             return render_template('login.html', errorMassage=errorMassage)
+        else:
+            session['user']='user'
+            return "gi"
+    else:
+        return render_template('login.html', errorMassage=" ")
+        
+
+        
+
 
 
 
