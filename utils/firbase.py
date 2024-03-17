7db6002c4fdf8fda75c3842d7535a432320b37ce
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-03-17 21:24:43 +0530

    setup firebase real time database

---
+++ utils/firbase.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import db, credentials
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+class Firebase(object):
+    def __init__(self) -> None:
+        self.cred=credentials.Certificate('agrisage-85205-firebase-adminsdk-6ih1w-881ad47372.json')
+        firebase_admin.initialize_app(self.cred, {'databaseURL' : os.getenv('FIREBASE_URL')})
+        
+    def getValue(self, key='/'):
+        if(key == '/'):
+            value=db.reference(key).get()
+        else:
+            value=db.reference(key).get()
+        return value
+        
+            
+
