c4a687b3957588ceb1e4c022669c6c4decd13194
= <pinildissanayaka@gmail.com>
2024-06-30 22:17:00 +0530

    add model weights

--- utils/prediction.py
+++ utils/prediction.py
@@ -1,14 +1,28 @@
+import os
 import pickle
+import logging
 
+
+logging.basicConfig(filename='logging')
 class Prediction(object):
     def __init__(self) -> None:
-        pass
+        self.modelPath=f'xception.weights.best.hdf5'
+        if not os.path.exists(self.modelPath):
+            logging.warning("Model doesn't exists")
+            
     
-    def loadModel():
-        pass
+    def loadModel(self):
+        with open(self.modelPath, 'rb') as openedModel:
+            model = pickle.load(openedModel)
+        return model
+
     
-    def preprocessImage():
-        pass
+    def preprocessImage(self):
+        model=self.loadModel()
+        pred = model.predict_generator(test_generator,
+                               steps=STEP_SIZE_TEST,
+                               verbose=1)
+        pred_classes = np.argmax(pred, axis=1)
         
-    def makePrediction():
+    def makePrediction(self):
         pass
