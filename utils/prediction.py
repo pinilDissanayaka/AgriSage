fa4f6ee49eeaba8e0b3135c74122f477843b8d48
= <pinildissanayaka@gmail.com>
2024-07-14 22:04:46 +0530

    add prediction

--- utils/prediction.py
+++ utils/prediction.py
@@ -1,4 +1,11 @@
 import pickle
+import tensorflow as tf
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+import warnings
+warnings.filterwarnings(action="ignore")
 
 class Prediction(object):
     def __init__(self) -> None:
@@ -6,15 +13,26 @@ class Prediction(object):
     
     def loadModel():
         with open('model_labels.pkl', 'rb') as labelPickle:
-            modelLabels=pickle.load(labelPickle)
+            modelLabels=pickle.load(labelPickle)[0]
             
-        with open('model.pkl', 'rb') as modelPickle:
-            model=pickle.load(modelPickle)
-            
-        return modelLabels, modelPickle
-            
-    def preprocessImage(imageFile):
-        pass
+        model=load_model('model.h5')
         
-    def makePrediction():
-        pass
+        return modelLabels, model
+            
+    def preprocessImage(self, imageFile):
+        image=np.array(Image.open(imageFile).convert('RGB').resize(224, 224))
+        image=image/255
+        imageArray=tf.expand_dims(image, 0)
+        return imageArray
+        
+    def makePrediction(self, imageFile):
+        modelLabels, model=self.loadModel()
+        imageArray=self.preprocessImage(imageFile=imageFile)
+        
+        prediction=model.predict(imageArray)
+        prediction=modelLabels[np.argmax(prediction[0])]
+        confidence=round(100 * (np.max(prediction[0])), 2)
+        
+        return prediction, confidence
+        
+        
